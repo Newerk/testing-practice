69ff845f5348f725357abb557dab9d37f80a3033
newerk <juniusowens@yahoo.com>
2023-04-04 16:38:49 -0400

    Add Test Cases for caesarCipher()

--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,16 +1,21 @@
 test('no shift (+/-0)', () => {
-    expect()
+    expect(caesarCipher('abcDEFghi', 0)).toMatch(/abcDEFghi/);
+    expect(caesarCipher('jklmnopqrs', 0)).toMatch(/jklmnopqrs/);
+    expect(caesarCipher('tu vwx yz', 0)).toMatch(/tu vwx yz/);
+    expect(caesarCipher('ONE 2 THR33 4OUR', 0)).toMatch(/ONE 2 THR33 4OUR/);
 })
 
 test('positive shift (+)', () => {
-    expect()
-    expect()
-    expect()
+    expect(caesarCipher('abc', 3)).toMatch(/def/);
+    expect(caesarCipher('tu vwx yz', 10)).toMatch(/de fgh ij/);
+    expect(caesarCipher('ONE 2 THR33 4OUR', 25)).toMatch(/NMD 2 SHQ33 4NTQ/);
+    expect(caesarCipher('z', 1)).toMatch(/a/);
 })
 
 test('negative shift (-)', () => {
-    expect()
-    expect()
-    expect()
+    expect(caesarCipher('abc', -3)).toMatch(/xyz/);
+    expect(caesarCipher('tu hj mw', -10)).toMatch(/jk xz cm/);
+    expect(caesarCipher('ONE 2 THR33 4OUR', -25)).toMatch(/EDU 2 JXH33 4EKH/);
+    expect(caesarCipher('a', -1)).toMatch(/z/);
 })
 
