36a91e9136d573507b98a71f0d84f51d2b1ad1e7
newerk <juniusowens@yahoo.com>
2023-04-05 16:26:27 -0400

    Subject line (try to keep under 50 characters)
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,4 +1,9 @@
 export function analyzeArray(array) {
+
+    if (array.length < 1) {
+        throw new Error('Array is empty')
+    }
+
     const object = {};
     object.average = array.reduce((prev, current) => prev + current, 0) / array.length;
     object.min = Math.min(...array);
