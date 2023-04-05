3a2e679fdc231fd894549267a4f67e9ace29833f
newerk <juniusowens@yahoo.com>
2023-04-05 16:26:26 -0400

    Add More Test Cases
    Taken into account situtations where the array is empty, of only has 1 element

--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -13,3 +13,15 @@ test('Max', () => {
 test('Length', () => {
     expect(analyzeArray(testArray).length).toBe(6)
 })
+
+test('Empty array', () => {
+    expect(() => analyzeArray([])).toThrowError();
+})
+
+test('Size 1 array', () => {
+    const arr = [42]
+    expect(analyzeArray(arr).average).toBe(42);
+    expect(analyzeArray(arr).min).toBe(42);
+    expect(analyzeArray(arr).max).toBe(42);
+    expect(analyzeArray(arr).length).toBe(1)
+})
